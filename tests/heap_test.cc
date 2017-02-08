1034c781cd7cc9df6ba154e0f3ab7052c3cc9be9
Marcin Panasiuk <panasiuk.marcin@gmail.com>
2017-02-08 11:10:31 +0100

    Add pop heap tests.

--- tests/heap_test.cc
+++ tests/heap_test.cc
@@ -1,17 +1,16 @@
 #include <stdio.h>
 #include "../heap.h"
-
-#define ASSERT(expr, msg) if (!(expr)) { printf("%s:%d: %s\n", __FILE__, __LINE__, (msg)); }
+#include "test.h"
 
 void checkHeapSize() {
     MinHeap<int> heap;
-    ASSERT(heap.size() == 0, "Empty heap size not equal 0.");
+    TEST(heap.size() == 0, "Empty heap size not equal 0.");
     heap.push(4);
-    ASSERT(heap.size() == 1, "Incorrect heap size.");
+    TEST(heap.size() == 1, "Incorrect heap size.");
     heap.push(4);
-    ASSERT(heap.size() == 2, "Incorrect heap size.");
+    TEST(heap.size() == 2, "Incorrect heap size.");
     heap.push(5);
-    ASSERT(heap.size() == 3, "Incorrect heap size.");
+    TEST(heap.size() == 3, "Incorrect heap size.");
     heap.push(6);
     heap.push(7);
     heap.push(6213);
@@ -22,51 +21,51 @@ void checkHeapSize() {
     heap.push(-1236);
     heap.push(-12226);
     heap.push(12326);
-    ASSERT(heap.size() == 13, "Incorrect heap size.");
+    TEST(heap.size() == 13, "Incorrect heap size.");
     heap.popAndPush(423);
-    ASSERT(heap.size() == 13, "Incorrect heap size.");
+    TEST(heap.size() == 13, "Incorrect heap size.");
     heap.popAndPush(41233);
-    ASSERT(heap.size() == 13, "Incorrect heap size.");
+    TEST(heap.size() == 13, "Incorrect heap size.");
 }
 
 void checkMinHeapTop() {
     MinHeap<int> heap;
     heap.push(5);
-    ASSERT(heap.top() == 5, "Incorrect min heap top.");
+    TEST(heap.top() == 5, "Incorrect min heap top.");
     heap.push(1);
-    ASSERT(heap.top() == 1, "Incorrect min heap top.");
+    TEST(heap.top() == 1, "Incorrect min heap top.");
     heap.push(10);
-    ASSERT(heap.top() == 1, "Incorrect min heap top.");
+    TEST(heap.top() == 1, "Incorrect min heap top.");
     heap.push(14);
-    ASSERT(heap.top() == 1, "Incorrect min heap top.");
+    TEST(heap.top() == 1, "Incorrect min heap top.");
     heap.push(0);
-    ASSERT(heap.top() == 0, "Incorrect min heap top.");
+    TEST(heap.top() == 0, "Incorrect min heap top.");
     heap.push(-7);
-    ASSERT(heap.top() == -7, "Incorrect min heap top.");
+    TEST(heap.top() == -7, "Incorrect min heap top.");
     heap.push(-7);
-    ASSERT(heap.top() == -7, "Incorrect min heap top.");
+    TEST(heap.top() == -7, "Incorrect min heap top.");
 }
 
 void checkMaxHeapTop() {
     MaxHeap<int> heap;
     heap.push(5);
-    ASSERT(heap.top() == 5, "Incorrect max heap top.");
+    TEST(heap.top() == 5, "Incorrect max heap top.");
     heap.push(1);
-    ASSERT(heap.top() == 5, "Incorrect max heap top.");
+    TEST(heap.top() == 5, "Incorrect max heap top.");
     heap.push(0);
-    ASSERT(heap.top() == 5, "Incorrect max heap top.");
+    TEST(heap.top() == 5, "Incorrect max heap top.");
     heap.push(14);
-    ASSERT(heap.top() == 14, "Incorrect max heap top.");
+    TEST(heap.top() == 14, "Incorrect max heap top.");
     heap.push(-7);
-    ASSERT(heap.top() == 14, "Incorrect max heap top.");
+    TEST(heap.top() == 14, "Incorrect max heap top.");
     heap.push(10);
-    ASSERT(heap.top() == 14, "Incorrect max heap top.");
+    TEST(heap.top() == 14, "Incorrect max heap top.");
     heap.push(0);
-    ASSERT(heap.top() == 14, "Incorrect max heap top.");
+    TEST(heap.top() == 14, "Incorrect max heap top.");
     heap.push(-7);
-    ASSERT(heap.top() == 14, "Incorrect max heap top.");
+    TEST(heap.top() == 14, "Incorrect max heap top.");
     heap.push(23);
-    ASSERT(heap.top() == 23, "Incorrect max heap top.");
+    TEST(heap.top() == 23, "Incorrect max heap top.");
 }
 
 void checkMinHeapPopAndPush() {
@@ -74,11 +73,11 @@ void checkMinHeapPopAndPush() {
     heap.push(45);
     heap.push(32);
     int result = heap.popAndPush(66);
-    ASSERT(result == 32, "Incorrect result of popAndPush().");
-    ASSERT(heap.top() == 45, "Incorrect heap top after popAndPush().");
+    TEST(result == 32, "Incorrect result of popAndPush().");
+    TEST(heap.top() == 45, "Incorrect heap top after popAndPush().");
     result = heap.popAndPush(21);
-    ASSERT(result == 45, "Incorrect result of popAndPush().");
-    ASSERT(heap.top() == 21, "Incorrect heap top after popAndPush().");
+    TEST(result == 45, "Incorrect result of popAndPush().");
+    TEST(heap.top() == 21, "Incorrect heap top after popAndPush().");
 }
 
 void checkMaxHeapPopAndPush() {
@@ -86,11 +85,35 @@ void checkMaxHeapPopAndPush() {
     heap.push(45);
     heap.push(32);
     int result = heap.popAndPush(66);
-    ASSERT(result == 45, "Incorrect result of popAndPush().");
-    ASSERT(heap.top() == 66, "Incorrect heap top after popAndPush().");
+    TEST(result == 45, "Incorrect result of popAndPush().");
+    TEST(heap.top() == 66, "Incorrect heap top after popAndPush().");
     result = heap.popAndPush(21);
-    ASSERT(result == 66, "Incorrect result of popAndPush().");
-    ASSERT(heap.top() == 32, "Incorrect heap top after popAndPush().");
+    TEST(result == 66, "Incorrect result of popAndPush().");
+    TEST(heap.top() == 32, "Incorrect heap top after popAndPush().");
+}
+
+void checkMinHeapPop() {
+    MinHeap<int> heap;
+    heap.push(23);
+    heap.push(41);
+    heap.push(56);
+    heap.push(71);
+    heap.push(11);
+    TEST(heap.top() == 11, "Incorrect heap top.");
+    auto popped = heap.pop();
+    TEST(popped == 11, "Incorrect heap pop() result.");
+    TEST(heap.top() == 23, "Incorrect heap top.");
+    popped = heap.pop();
+    TEST(popped == 23, "Incorrect heap pop() result.");
+    TEST(heap.top() == 41, "Incorrect heap top.");
+    heap.push(91);
+    heap.push(111);
+    popped = heap.pop();
+    TEST(popped == 41, "Incorrect heap pop() result.");
+    TEST(heap.top() == 56, "Incorrect heap top.");
+    popped = heap.pop();
+    TEST(popped == 56, "Incorrect heap pop() result.");
+    TEST(heap.top() == 71, "Incorrect heap top.");
 }
 
 int main() {
@@ -99,5 +122,6 @@ int main() {
     checkMaxHeapTop();
     checkMinHeapPopAndPush();
     checkMaxHeapPopAndPush();
+    checkMinHeapPop();
     return 0;
 }
