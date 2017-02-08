14cca622457f99ba48d27c1232b1551b53dcc2bf
Marcin Panasiuk <panasiuk.marcin@gmail.com>
2017-02-08 21:10:45 +0100

    Add solution_sorted.

--- Makefile
+++ Makefile
@@ -2,9 +2,10 @@ CCFLAGS=-std=c++11 -Wall -O2
 
 .PHONY: tests solutions
 
-solutions: array.h heap.h solution_heaps.cc solution_quickselect.cc
-	g++ $(CCFLAGS) solution_heaps.cc -o bin/solution_heaps
+solutions: array.h heap.h solution_heaps.cc solution_quickselect.cc solution_sorted.cc
+	g++ $(CCFLAGS) solution_sorted.cc -o bin/solution_sorted
 	g++ $(CCFLAGS) solution_quickselect.cc -o bin/solution_quickselect
+	g++ $(CCFLAGS) solution_heaps.cc -o bin/solution_heaps
 
 tests: array.h heap.h tests/heap_test.cc
 	g++ $(CCFLAGS) tests/heap_test.cc -o bin/heap_test
---
+++ solution_sorted.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <iomanip>
+#include "array.h"
+
+size_t last_smaller_index(const Array<int>& array, int elem) {
+    for (size_t i = 0; i < array.size(); ++i) {
+        if (array[i] >= elem) {
+            return i;
+        }
+    }
+    return array.isEmpty() ? 0 : array.size();
+}
+
+void compute_median(std::istream& in, std::ostream& os) {
+    int number;
+    Array<int> numbers;
+    while (in >> number) {
+        size_t insert_index = last_smaller_index(numbers, number);
+        numbers.insert(number, insert_index);
+
+        double median = 0.0;
+        if (numbers.size() % 2) {
+            median = numbers[numbers.size() / 2];
+        } else {
+            median = numbers[numbers.size() / 2 - 1];
+            median += numbers[numbers.size() / 2];
+            median /= 2.0;
+        }
+
+        os << std::setprecision(12) << median << std::endl;
+    }
+}
+
+int main() {
+    std::istream in(std::cin.rdbuf());
+    std::ostream out(std::cout.rdbuf());
+    compute_median(in, out);
+}
